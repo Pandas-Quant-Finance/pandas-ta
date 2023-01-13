5c5d5dad74ce89db9f7355dae94532202290a50a
KIC <KIC@KIC.KIC>
2023-01-13 14:58:55 +0000

    allow numpy arrays as result

--- pandas-ta/pandas_ta/technical_analysis/indicators/custom_indicators.py
+++ pandas-ta/pandas_ta/technical_analysis/indicators/custom_indicators.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 
+from pandas_df_commons.extensions.functions import rolling_apply
 from pandas_df_commons.indexing import get_columns
 from pandas_df_commons.indexing.decorators import foreach_column, foreach_top_level_row, rename_with_parameters
 
@@ -21,3 +23,41 @@ def ta_gmean(df: pd.Series, period, real='Close'):
 
     return wrapped(data, period)
 
+
+@foreach_top_level_row
+def ta_mamentum(df: pd.Series, period, real='Close', mas=None):
+    data = get_columns(df, real) if real is not None and df.ndim > 1 else df
+
+    class Object(object):
+        def __init__(self, value):
+            self.value = value
+
+    @foreach_column
+    @rename_with_parameters(function_name='mamemntum', parameter_names=['period', 'mas'], output_names=["momentum", "overdrive", "drag"])
+    def wrapped(s, period, mas):
+        mas = np.linspace(period - 1, 0, mas, endpoint=True, dtype=int)
+        scale = (len(mas) / 2) - 1
+
+        def mamentum_basis(window):
+            means = []
+            sum = 0
+
+            for i in mas:
+                sum += float(window.iloc[i])
+                means.append(sum / (len(means) + 1))
+
+            return Object(np.array(means))
+
+        mmeans = rolling_apply(s, 60, mamentum_basis)
+        return ["momentum", "overdrive", "drag"], pd.concat(
+            [
+                s.to_frame()[[]],
+                rolling_apply(mmeans, 2, lambda x: np.sum(x.iloc[-1, 0].value >= x.iloc[0, 0].value) / scale),
+                mmeans.apply(lambda x: x[0].value[-1] / x[0].value[0] - 1, axis=1),
+                mmeans.apply(lambda x: np.abs(x[0].value[0] - x[0].value[1:]).argmin(), axis=1)
+            ],
+            axis=1,
+            join='outer'
+        ).values
+
+    return wrapped(data, period, mas if mas is not None else period)
--- test-pandas-ta/test_pandas_ta/indicators/test_custom_indicators.py
+++ test-pandas-ta/test_pandas_ta/indicators/test_custom_indicators.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import pandas as pd
 
 from config import DF_TEST_MULTI_ROW_MULTI_COLUMN as DF_TEST
-from pandas_ta.technical_analysis import ta_gmean
+from pandas_ta.technical_analysis import ta_gmean, ta_mamentum
 
 
 class TestCustomIndicators(TestCase):
@@ -14,3 +14,7 @@ class TestCustomIndicators(TestCase):
 
         pd.testing.assert_frame_equal(df1.loc["A"], df1.loc["B"])
         #pd.testing.assert_frame_equal(df2.loc["A"], df2.loc["B"])
+
+    def test_mamentum(self):
+        mam = ta_mamentum(DF_TEST, 40, mas=20)
+        print(mam)
