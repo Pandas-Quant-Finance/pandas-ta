4106badb6daf33ced9aa1f817c1064f5a57739e4
KIC <KIC@KIC.KIC>
2023-08-03 09:03:03 +0200

    fix shapes

--- pandas-ta/pandas_ta/technical_analysis/indicators/change_proint_indicators.py
+++ pandas-ta/pandas_ta/technical_analysis/indicators/change_proint_indicators.py
@@ -7,14 +7,14 @@ from pandas_df_commons.extensions.functions import rolling_apply
 from pandas_df_commons.indexing.decorators import foreach_top_level_row, foreach_column
 
 
-@foreach_top_level_row
 @foreach_column
+@foreach_top_level_row
 def ta_std_ratio(df: pd.DataFrame, slow: int, fast: int, relative_loc: bool = False) -> pd.DataFrame:
     def change_point(df):
         tmp = np.abs(((df.rolling(fast).std() / df.iloc[-slow:].std()) - 1)).dropna()
         return {
-            "loc": (np.argmax(np.abs(tmp)) / slow) if relative_loc else tmp.index[np.argmax(tmp)],
-            "score": np.max(tmp)
+            f"loc_{slow}_{fast}": (np.argmax(np.abs(tmp)) / slow) if relative_loc else tmp.index[np.argmax(tmp)],
+            f"score_{slow}_{fast}": np.max(tmp)
         }
 
     return rolling_apply(df, slow+fast, change_point)
